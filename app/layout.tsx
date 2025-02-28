88183839f0630330c44630f28da5d3e946e96db5
meredith <dmzart921@gmail.com>
2025-02-28 11:53:13 +0200

    v0.2.9: proper metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,62 @@ const geist = Geist({
 export const metadata: Metadata = {
   title: "Openstud",
   description: "The Ultimate Place for Smarter Learning",
+  metadataBase: new URL("https://openstud.vercel.app/"),
+  keywords: [
+    "education",
+    "smarter",
+    "learning",
+    "openstud",
+    "study",
+    "study management",
+    "revision",
+    "revision assistant",
+    "study planning",
+    "progress tracking",
+    "study resources",
+    "analytics",
+    "community support",
+    "student",
+    "success",
+    "focus mode",
+    "collaboration",
+    "study groups",
+    "custom templates",
+    "ai",
+    "perplexity ai",
+    "sonar",
+    "cuttypie",
+  ],
+  authors: [
+    {
+      name: "cuttypie",
+      url: "https://cuttypiedev.vercel.app/",
+    },
+  ],
+  creator: "cuttypie",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://openstud.vercel.app/",
+    title: "Openstud",
+    description: "The Ultimate Place for Smarter Learning",
+    siteName: "Openstud",
+    images: [
+      {
+        url: "https://openstud.vercel.app/openstud-logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Openstud",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Openstud",
+    description: "The Ultimate Place for Smarter Learning",
+    images: ["https://openstud.vercel.app/openstud-logo.png"],
+    creator: "@DorianTho5",
+  },
 };
 
 export default function RootLayout({
