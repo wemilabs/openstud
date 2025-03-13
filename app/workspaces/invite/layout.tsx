ff50ead84d893842f85e6cc24bfbf7c5246871f6
meredith <dmzart921@gmail.com>
2025-03-13 20:35:02 +0200

    v0.5.19: okay now with metadata for the invite link

--- app/workspaces/invite/layout.tsx
+++ app/workspaces/invite/layout.tsx
@@ -11,6 +11,30 @@ export const metadata: Metadata = {
     "collaboration",
     "study process",
   ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://openstud.vercel.app/",
+    title: "Workspace Invitation - Openstud",
+    description: "You're being invited to join a workspace.",
+    siteName: "Openstud",
+    images: [
+      {
+        url: "https://ubrw5iu3hw.ufs.sh/f/TFsxjrtdWsEIpmbaYUVIN8qB9zrPkx7h3YEvXTfboyKpdS2L",
+        width: 1200,
+        height: 630,
+        alt: "Openstud",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Workspace Invitation - Openstud",
+    description: "You're being invited to join a workspace.",
+    images: [
+      "https://ubrw5iu3hw.ufs.sh/f/TFsxjrtdWsEIpmbaYUVIN8qB9zrPkx7h3YEvXTfboyKpdS2L",
+    ],
+  },
 };
 
 export default function InviteLayout({
