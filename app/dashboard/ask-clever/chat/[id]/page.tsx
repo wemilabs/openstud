40f4609238f6657c3bff6309af9e554f6f6915e8
meredith <dmzart921@gmail.com>
2025-04-01 17:52:05 +0200

    dumb fix

---
+++ app/dashboard/ask-clever/chat/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function ChatPage({ params }: { params: { id: string } }) {
+  return <div>Chat</div>;
+}
---
+++ app/dashboard/ask-clever/chat/new/page.tsx
@@ -0,0 +1,3 @@
+export default function NewChatPage() {
+  return <div>New Chat</div>;
+}
