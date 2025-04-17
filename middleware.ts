03656854f2df238858062aa9724526525725c91c
meredith <dmzart921@gmail.com>
2025-04-17 13:24:38 +0200

    Add debug logging and simplify token retrieval in authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! });
+  const token = await getToken({ req });
   const isLoggedIn = !!token;
+
+  console.log(req.cookies);
   console.log(`isLoggedIn: ${isLoggedIn}`);
   console.log(token);
 
