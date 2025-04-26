880d183e44891230906e24e2c20eb0c1fe59f26f
meredith <dmzart921@gmail.com>
2025-04-26 15:30:58 +0200

    Conditional rendering of dashboard nav items

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { MyHoverCard } from "@/components/sections/my-hover-card";
 
 export function Hero() {
   return (
     <section className="relative overflow-hidden min-h-screen pt-24 md:pt-30">
       <div className="container relative mx-auto">
-        <div className="mx-auto flex max-w-[980px] flex-col items-center gap-6 md:gap-y-10 text-center">
+        <div className="mx-auto flex max-w-[980px] flex-col items-center gap-6 md:gap-y-10 text-center px-6 md:px-0">
           <Badge
             variant="outline"
             className="bg-blue-100 dark:bg-blue-950 text-blue-600 dark:text-blue-400 hover:bg-blue-100 dark:hover:bg-blue-950 rounded-full px-3 py-2 text-xs md:text-sm leading-6 tracking-wide border-none shadow-none"
@@ -32,13 +31,12 @@ export function Hero() {
             >
               Get Started for Free
             </Link>
-            <Button
-              variant="outline"
-              size="lg"
-              className="shadow-xs shadow-blue-500 hover:bg-blue-100 transition duration-200 dark:hover:text-muted cursor-not-allowed"
+            <Link
+              href="/docs/introduction"
+              className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-10 px-6 has-[>svg]:px-4 text-accent-foreground dark:text-foreground shadow-blue-500 hover:bg-blue-100 transition duration-200 dark:hover:text-muted border border-input bg-background shadow-xs hover:text-accent-foreground"
             >
               Documentation
-            </Button>
+            </Link>
           </div>
         </div>
       </div>
